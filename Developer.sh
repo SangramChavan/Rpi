2e55da9f6a72db9f1574ce3ffe41cf342cbfcde3
Sangram Chavan <sangramchavan16@gmail.com>
2016-12-29 01:41:33 +0530

    Update Developer.sh

--- Developer.sh
+++ Developer.sh
@@ -1,11 +1,14 @@
-sudo apt install yum
-sudo apt-get install curl
-curl --silent --location https://deb.nodesource.com/setup_5.x | sudo bash -
-sudo apt-get install nodejs
- 
-sudo npm install -g cordova
+sudo apt-get update
 
-sudo npm install -g ionic
+sudo apt install yum -y
+sudo apt-get install curl -y
 
-sudo apt-get install git
-sudo apt-get install docker-engine
+sudo apt install vim -y                                 #Vim update
+sudo apt-get install build-essential -y
+sudo add-apt-repository ppa:plushuang-tw/uget-stable -y
+sudo apt install uget -y                                #Installing Download Manager
+sudo apt-get install firefox -y                         #Firefox
+sudo apt-get install vlc browser-plugin-vlc -y          #VLC player
+sudo apt-get remove totem-gstreamer totem-common -y     #Remove Videos
+sudo apt-get install xclip                              #Required to generate a SSH key for GitHub
+curl -sSL https://get.docker.com/ | sh                  #Docker
