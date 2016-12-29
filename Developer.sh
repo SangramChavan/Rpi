02a325925515f8dd9f43b75f18e5cf732f4c03a8
Sangram Chavan <sangramchavan16@gmail.com>
2016-12-29 17:53:11 +0530

    Update Developer.sh

--- Developer.sh
+++ Developer.sh
@@ -12,3 +12,9 @@ sudo apt-get install vlc browser-plugin-vlc -y          #VLC player
 sudo apt-get remove totem-gstreamer totem-common -y     #Remove Videos
 sudo apt-get install xclip                              #Required to generate a SSH key for GitHub
 curl -sSL https://get.docker.com/ | sh                  #Docker
+wget https://nodejs.org/dist/v6.9.2/node-v6.9.2-linux-x64.tar.xz  #Download Nodejs
+mv node-v6.9.2-linux-x64 node
+sudo cp -r node/{bin,include,lib,share} /usr/
+node -v
+sudo npm install -g cordova
+sudo npm install -g ionic
