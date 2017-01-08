fae69b73a2a430410ce27abcb3446090e83503ae
Sangram Chavan <sangramchavan16@gmail.com>
2017-01-08 20:10:24 +0530

    Update Developer.sh

--- Developer.sh
+++ Developer.sh
@@ -22,6 +22,7 @@ sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql -y             
 sudo apt-get install phpmyadmin -y                      #phpmyadmin
 sudo apt install composer -y                            #composer
 sudo apt-get install ruby-full -y                       #Ruby
+sudo apt install ruby-bundler  -y                       #ruby-bundler
 
 #Node
 wget https://nodejs.org/dist/v6.9.2/node-v6.9.2-linux-x64.tar.xz  #Download Nodejs
